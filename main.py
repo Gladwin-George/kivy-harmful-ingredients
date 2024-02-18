275724f5c6a48d68d90d5df0bc5fa162ada1a870
Gladwin George <gladwingeorge189@gmail.com>
2024-02-18 23:41:37 +0530

    removed unwanted codes

--- main.py
+++ main.py
@@ -214,18 +214,6 @@ class UserManager:
 from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
 
-# class BgLabel(Label):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         with self.canvas.before:
-#             Color(1, 0, 0, 1)  # colors range from 0-1 instead of 0-255
-#             self.rect = Rectangle(size=self.size, pos=self.pos)
-#         self.bind(size=self._update_rect, pos=self._update_rect)
-
-#     def _update_rect(self, instance, value):
-#         self.rect.pos = instance.pos
-#         self.rect.size = instance.size
-
 
 class OCRApp(App):
 
